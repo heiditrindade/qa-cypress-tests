2ac8fdaf4598271df44a6f16b855d5681191ac3f
Heidi Trindade <heidi.trindade@gmail.com>
2025-02-04 21:06:05 -0300

    Add Cart Tests

---
+++ cypress/e2e/desafio-qa/cart.cy.js
@@ -0,0 +1,31 @@
+import CartPage from '../../pages/CartPage';
+import SigninPage from '../../pages/SigninPage';
+
+describe('Cart Tests', () => {
+    const signinPage = new SigninPage();
+    const cartPage = new CartPage();
+
+    beforeEach(() => {  
+        signinPage.visit();
+        signinPage.fillEmail('bacev49929@andinewS.com');
+        signinPage.fillPassword('entrar123');
+        signinPage.submitButton();
+        signinPage.assertLoginSuccess();
+    });
+
+    it('deve adicionar um item ao carrinho', () => {
+        cartPage.visit();
+        cartPage.addItem();
+        cartPage.assertItemInCart();
+    });
+
+    it('deve remover um item do carrinho', () => {
+        cartPage.visit();
+        cartPage.addItem();
+        cartPage.assertItemInCart();
+        cartPage.checkoutButton();
+        cartPage.deleteItemFromCart();
+        cartPage.assertItemNotInCart();
+    });
+
+});
